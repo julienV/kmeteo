771f86d876b5966c7c7812d1ed0438c74597d0ac
Julien Vonthron <julien.vonthron@gmail.com>
2018-10-16 14:33:32 +0200

    get position

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  position: {lat: number, lng: number};
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
   }
 
+  ionViewDidLoad() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log(resp.coords);
+      this.position = {lat: resp.coords.latitude, lng: resp.coords.longitude};
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
 }
